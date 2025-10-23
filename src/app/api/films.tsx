363fef39b74877c84c4d24597de18003c1d4ecce
Juan-Vera-ok <juanvera2001@hotmail.com>
2025-10-23 02:26:20 -0300

    First commit

---
+++ src/app/api/films.tsx
@@ -0,0 +1,18 @@
+const images = ["/img/films/film1.jpg", "/img/films/film2.jpg", "/img/films/film3.jpg", "/img/films/film4.jpg", "/img/films/film5.jpg", "/img/films/film6.jpg"];
+type Film = { id: string; title: string; body: string; image: string };
+
+
+
+async function getFilms(): Promise<Film[]> {
+  const res = await fetch('https://swapi.dev/api/films/', { next: { revalidate: 60 } })
+  if (!res.ok) throw new Error('Failed to fetch posts')
+  const data = await res.json()
+  return (data.results || []).map((f: any, index: number) => ({
+    id: String(f.episode_id),
+    title: f.title,
+    body: f.opening_crawl,
+    image: images[index] || null,
+  }))
+}
+
+export { getFilms };
